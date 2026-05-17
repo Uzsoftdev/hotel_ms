b4de811f7fa3a13944de01fbc6178ac5f9e481d8
Sunnat Axmadov <sunnatakhmad@gmail.com>
2026-05-17 20:47:44 +0500

    fix: remove unused sa import in migration 0005

--- backend/migrations/versions/20260428_0005_partition_bookings.py
+++ backend/migrations/versions/20260428_0005_partition_bookings.py
@@ -17,7 +17,6 @@ Create Date: 2026-04-28 00:05:00.000000
 from typing import Sequence, Union
 
 from alembic import op
-import sqlalchemy as sa
 
 revision: str = "20260428_0005"
 down_revision: Union[str, None] = "20260428_0004"
